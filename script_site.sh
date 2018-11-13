e04f23fcad03f0dfd29a084b7fc31413ab9c76d1
root <root@localhost.localdomain>
2018-11-13 13:59:05 +0300

    Add scripts_image

--- script_site.sh
+++ script_site.sh
@@ -10,12 +10,29 @@ for i in $n
 do
 echo "$i" >>city
 done
-
-m=`$city`
+c=0
+m=`cat city`
+mkdir Download
+rm -f png
+touch png
+rm -f site_png
+touch site_png
 for i in $m
 do
+echo $i
 curl $i > site_png
-m=`grep -o "https://postcode.in.ua/images/image_c.php?id=[0-9]*" site_png`
-wget -P /Download/$m
-
+grep -o "https://postcode.in.ua/images/image_c.php?id=[0-9]*[0-9]" site_png >> png
 done
+m=`cat png`
+c=0
+for i in $m
+do
+#!wget $i
+let c+=1
+ wget -P /root/Linux/Download/ $i 
+mv /root/Linux/Download/image*  /root/Linux/Dorwnload/img$c.png
+done
+git add -A
+git commit -m "Add scripts_image"
+git push
+
